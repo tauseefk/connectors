30b9e667dc2b3eafc87c51c0de525914881267e4
Tauseef Khan <tauseef25@gmail.com>
2025-08-16 22:00:34 -0700

    return board data and render HTML elements

--- connecto.rs/connectors/src/lib.rs
+++ connecto.rs/connectors/src/lib.rs
@@ -1,44 +1,10 @@
-#![allow(dead_code)]
-
 mod engine;
+mod utils;
 
 mod prelude {
     pub use std::fmt;
 
     pub use wasm_bindgen::prelude::wasm_bindgen;
-}
 
-use engine::TileType;
-use prelude::*;
-
-static ROWS_COUNT: usize = 6;
-static COLS_COUNT: usize = 7;
-
-/// Get n random values upto but not including k
-fn get_random_values(n: usize, k: u8) -> Vec<u8> {
-    let mut values = Vec::with_capacity(n);
-    for _ in 0..n {
-        values.push((js_sys::Math::random() * (k as f64)).floor() as u8);
-    }
-    values
-}
-
-fn pretty_print_grid(board: &Vec<TileType>) {
-    let mut output = String::new();
-    board.iter().enumerate().for_each(|(i, tile)| {
-        if i > 0 && i % COLS_COUNT == 0 {
-            output.push('\n');
-        }
-        output.push_str(&format!("{} ", tile));
-    });
-    output.push('\n');
-    web_sys::console::log_1(&output.into());
-}
-
-#[wasm_bindgen]
-pub fn say_hello() {
-    // TileType has 3 variants, so we want 0, 1, and 2 as the values
-    let board = get_random_values(ROWS_COUNT * COLS_COUNT, 3);
-    let board: Vec<TileType> = board.iter().map(|&v| v.into()).collect();
-    pretty_print_grid(&board);
+    pub use crate::utils::*;
 }
