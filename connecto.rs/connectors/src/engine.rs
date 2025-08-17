440f5ce102138d976e0a1b45f6a763e6dae4eb96
Tauseef Khan <tauseef25@gmail.com>
2025-08-16 21:33:28 -0700

    rendering a grid

--- connecto.rs/connectors/src/engine.rs
+++ connecto.rs/connectors/src/engine.rs
@@ -9,12 +9,23 @@ pub enum TileType {
 impl fmt::Display for TileType {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         let tile_repr = match self {
-            TileType::Empty => "Empty",
+            TileType::Empty => "Empty Tile",
             TileType::Red => "Player Red",
             TileType::Black => "Player Black",
         };
 
         write!(f, "{tile_repr}")
+    }
+}
+
+impl From<u8> for TileType {
+    fn from(value: u8) -> Self {
+        match value {
+            b'_' | 0 => TileType::Empty,
+            b'R' | 1 => TileType::Red,
+            b'B' | 2 => TileType::Black,
+            _ => panic!("Unexpected character {value} found for tile."),
+        }
     }
 }
 
